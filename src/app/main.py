3d9ba2cffbb7f1f71de808308f196ffc6b81cf0a
rninizoo <rkdalswn1209@gmail.com>
2025-09-23 23:32:16 +0900

    fix: route

--- src/app/main.py
+++ src/app/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import RedirectResponse
 from sqlmodel import SQLModel
 
 from ..features.auth.router import router as auth_router
+from ..features.tests.router import router as test_router
 from ..features.users.router import router as user_router
 from ..shared.database import engine
 
@@ -18,3 +19,4 @@ def read_root():
 
 app.include_router(user_router)
 app.include_router(auth_router)
+app.include_router(test_router)
